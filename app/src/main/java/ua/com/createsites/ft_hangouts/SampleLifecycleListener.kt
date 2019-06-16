fe1113684d35289db109bc6b109c649639273721
ibohonos <ibohonos@e1r10p8.unit.ua>
2019-06-16 11:42:23 +0300

    fix view background time

--- app/src/main/java/ua/com/createsites/ft_hangouts/SampleLifecycleListener.kt
+++ app/src/main/java/ua/com/createsites/ft_hangouts/SampleLifecycleListener.kt
@@ -1,47 +1,35 @@
 package ua.com.createsites.ft_hangouts
 
-import android.annotation.SuppressLint
-import android.arch.lifecycle.Lifecycle
 import android.arch.lifecycle.LifecycleObserver
 import android.arch.lifecycle.OnLifecycleEvent
+import android.arch.lifecycle.Lifecycle
+import java.time.temporal.ChronoUnit
 import android.content.Context
-import android.util.Log
-import android.widget.Toast
-import java.text.DateFormat
-import java.text.SimpleDateFormat
-import java.time.LocalDate
 import java.time.LocalDateTime
-import java.util.*
+import android.widget.Toast
+import android.os.Build
 
 class SampleLifecycleListener(context: Context): LifecycleObserver {
 
-    private var backTime: Calendar? = null
-    private val myCon = context
-
+    private var backTime: LocalDateTime? = null
+    private val myCon: Context = context
 
     @OnLifecycleEvent(Lifecycle.Event.ON_START)
     fun onMoveToForeground() {
         if (backTime != null) {
-            val time1 = backTime.toString()
-            val time2 = Calendar.getInstance().toString()
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+                val now = LocalDateTime.now()
+                val diff = ChronoUnit.SECONDS.between(backTime, now)
 
-            val format = SimpleDateFormat(DateFormat, Locale.ROOT)
-            val date1 = format.parse(time1)
-            val date2 = format.parse(time2)
-            val difference = date2.getTime() - date1.getTime()
-//            val currTime = Calendar.getInstance().time.toString()
-//            val currTime = LocalDateTime.now().minus(backTime).toString()
-//            Toast.makeText(myCon, String.format(myCon.getString(R.string.background_message), currTime), Toast.LENGTH_LONG).show()
-            Log.d("SampleLifecycle", difference.toString())
+                Toast.makeText(myCon, String.format(myCon.getString(R.string.background_message), diff.toString()), Toast.LENGTH_SHORT).show()
+            }
         }
-        Log.d("SampleLifecycle", backTime.toString())
-        Log.d("SampleLifecycle", "Returning to foreground…")
     }
 
     @OnLifecycleEvent(Lifecycle.Event.ON_STOP)
     fun onMoveToBackground() {
-        backTime = Calendar.getInstance()
-//        backTime = LocalDateTime.now()
-        Log.d("SampleLifecycle", "Moving to background…")
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            backTime = LocalDateTime.now()
+        }
     }
 }
